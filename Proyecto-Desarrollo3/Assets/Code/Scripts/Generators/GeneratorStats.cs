52605fde16e8da3fdf9aa82108d3bba706cf469a
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2023-08-24 15:41:19 -0300

    Added GeneratorStats sciptable object

---
+++ Proyecto-Desarrollo3/Assets/Code/Scripts/Generators/GeneratorStats.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Code.Scripts.Generators
+{
+    [CreateAssetMenu(fileName = "GeneratorStats")]
+    public class GeneratorStats : ScriptableObject
+    {
+        [SerializeField] public float timerMax;
+        [SerializeField] public float levelUpCostIncrease;
+        [SerializeField] public double levelUpCost;
+        [SerializeField] public double currencyGenerated;
+        [SerializeField] public double currencyGeneratedIncrease;
+        [SerializeField] public string description;
+    }
+}
