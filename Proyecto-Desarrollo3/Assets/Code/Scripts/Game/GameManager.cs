8020a9415c2764ccfdc378d685932c969c14a12e
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2023-08-27 18:30:06 -0300

    Updated GameManager script

--- Proyecto-Desarrollo3/Assets/Code/Scripts/Game/GameManager.cs
+++ Proyecto-Desarrollo3/Assets/Code/Scripts/Game/GameManager.cs
@@ -3,8 +3,7 @@ using Code.Scripts.Generators;
 using Code.Scripts.Player;
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace Code.Scripts.Game
 {
@@ -17,10 +16,8 @@ namespace Code.Scripts.Game
         [SerializeField] private Generator[] generators;
         [SerializeField] private GeneratorStats playerGeneratorStats;
         [SerializeField] private GameObject textHolderPrefab;
+        [SerializeField] private Button[] generatorButtons;
 
-        private const short GeneratorsQty = 5;
-
-        private Generator _playerGenerator;
         private PlayerManager _playerManager;
         private PlayerView _playerView;
 
@@ -28,31 +25,55 @@ namespace Code.Scripts.Game
 
         public static event CurrencyEventHandler OnCurrencyEvent;
 
+        public delegate void GeneratorUpgradeEventHandler(short id, int cost);
+
+        public static event GeneratorUpgradeEventHandler OnGeneratorChangeEvent;
+
         private void Awake()
         {
             InitPlayerComponents();
+            if (PlayerPrefs.HasKey("currency"))
+            {
+                AddCurrency(PlayerPrefs.GetInt("currency"));
+            }
         }
+
         private void Update()
         {
-            foreach (Generator generator in generators)
+            for (int i = 0; i < generators.Length; i++)
             {
-                if (!generator.IsActive) continue;
-                double generated = generator.Generate();
+                if(i == 0) continue;
+                if (!generators[i].IsActive) continue;
+                double generated = generators[i].Generate();
                 if (generated <= 0) continue;
 
                 AddCurrency(generated);
             }
+
+            PlayerPrefs.SetInt("currency", (int)_playerManager.Wallet.Currency);
+            PlayerPrefs.Save();
         }
 
         private void InitPlayerComponents()
         {
-            _playerGenerator = gameObject.AddComponent<Generator>();
-            _playerGenerator.generatorStats =  playerGeneratorStats;
-            _playerGenerator.textHolderPrefab = textHolderPrefab;
             _playerManager = gameObject.AddComponent<PlayerManager>();
             _playerManager.Wallet = gameObject.AddComponent<Wallet>();
+
             _playerView = gameObject.AddComponent<PlayerView>();
             _playerView.CurrencyText = currencyText;
+            _playerView.wallet = _playerManager.Wallet;
+
+            _playerView.generatorsUiText = new TMP_Text[generatorButtons.Length];
+
+            for (int i = 0; i < generatorButtons.Length; i++)
+            {
+                _playerView.generatorsUiText[i] = generatorButtons[i].GetComponentsInChildren<TMP_Text>()[0];
+            }
+
+            for (int i = 0; i < generators.Length; i++)
+            {
+                OnGeneratorChangeEvent?.Invoke((short)i, (int)generators[i].generatorStats.levelUpCost);
+            }
         }
 
         private void AddCurrency(double currency)
@@ -67,34 +88,44 @@ namespace Code.Scripts.Game
 
         public void PlayerClick()
         {
-            double currencyGenerated = _playerGenerator.Generate();
-            if(currencyGenerated <= 0) return;
+            double currencyGenerated = generators[0].Generate();
+            if (currencyGenerated <= 0) return;
             AddCurrency(currencyGenerated);
-            
+
             Transform textTransform = transform;
             textTransform.position = Camera.main!.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 3;
-            
+
             GameObject textInstance = Instantiate(textHolderPrefab, textTransform);
-            
+
             textInstance.GetComponent<TextManager>().SetText(currencyGenerated.ToString(CultureInfo.InvariantCulture));
         }
 
         public void UpgradeLevel(int id)
         {
+            if (!generators[id]) return;
+            if(_playerManager.Wallet.Currency < generators[id].generatorStats.levelUpCost) return;
+            
+            if (!generators[id].IsActive)
+            {
+                UnlockGenerator(id);
+            }
+            else
+            {
+                RemoveCurrency(generators[id].Upgrade(_playerManager.Wallet.Currency));
+                OnGeneratorChangeEvent?.Invoke((short)id, (int)generators[id].generatorStats.levelUpCost);
+            }
+        }
+
+        [ContextMenu("Unlock Generator")]
+        public void UnlockGenerator(int id)
+        {
+            if (_playerManager.Wallet.Currency < generators[id].generatorStats.levelUpCost) return;
+
             RemoveCurrency(generators[id].Upgrade(_playerManager.Wallet.Currency));
+            generators[id].IsActive = true;
+            generators[id].gameObject.SetActive(true);
         }
 
-        public void UpgradePlayerGenerator()
-        {
-            RemoveCurrency(_playerGenerator.Upgrade(_playerManager.Wallet.Currency));
-        }
-
-        [ContextMenu("Unlock basic Generator")]
-        public void UnlockGenerator()
-        {
-            generators[0].IsActive = true;
-        }
-        
         [ContextMenu("Upgrade basic Generator")]
         public void UpgradeBasicGenerator()
         {
