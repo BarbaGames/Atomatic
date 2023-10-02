cd8fd8c2e3a90aabf254ac834cc4f450fb2992b3
A72\Aula 7 <barrasanti@gmail.com>
2023-10-02 12:51:43 -0300

    Added buttons controller

---
+++ Proyecto-Desarrollo3/Assets/Scripts/UI/ButtonsController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ButtonsController : MonoBehaviour
+    {
+        [SerializeField] private Button[] buttons;
+
+        public void EnableButton(int id)
+        {
+            buttons[id].gameObject.SetActive(true);
+        }
+
+        public void DisableButton(int id)
+        {
+            buttons[id].gameObject.SetActive(false);
+        }
+    }
+}
