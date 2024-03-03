fa273437a33b6575dbd8d83ff55f347bb3cff114
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-03-03 17:25:16 -0300

    Add animations to gen 4

---
+++ Proyecto-Desarrollo3/Assets/Scripts/Stage/MoveTanks.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Stage
+{
+    public class MoveTanks : MonoBehaviour
+    {
+        [SerializeField] private Animator[] animators;
+
+        private const float MaxTime = 11f;
+        private const int ActivationChance = 30;
+        private float timer = MaxTime;
+
+        private const string BoolKey = "Start";
+        private static readonly int StartKey = Animator.StringToHash(BoolKey);
+
+        private void Update()
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                timer = MaxTime;
+                StartCoroutine(StartAnimations());
+            }
+        }
+
+        private IEnumerator StartAnimations()
+        {
+            foreach (Animator animator in animators)
+            {
+                int rand = Random.Range(0, 100);
+                if (rand <= ActivationChance)
+                {
+                    animator.SetBool(StartKey, true);
+                }
+            }
+
+            yield return new WaitForSeconds(1.0f);
+
+            foreach (Animator animator in animators)
+            {
+                animator.SetBool(StartKey, false);
+            }
+        }
+    }
+}
